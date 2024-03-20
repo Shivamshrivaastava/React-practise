1c4f146efb072e873a5a8cfeb99dfbb0c699dec2
Shivamshrivaastava <shivamshrivaastava@gmail.com>
2024-03-20 21:09:16 +0530

    first-commit

--- Projects/src/App.js
+++ Projects/src/App.js
@@ -3,6 +3,8 @@ import Accordian from "./components/accordian";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import TabTest from "./components/custom-tabs/tab-test";
 import GithubProfileFinder from "./components/github-profile-finder";
+import ImageSlider from "./components/image-slider";
+import LightDarkMode from "./components/light-darker-mode";
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,12 @@ function App() {
       <ModalTest />
       <TabTest />
       <GithubProfileFinder />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+      <LightDarkMode />
     </div>
   );
 }
